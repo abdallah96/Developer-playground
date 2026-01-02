5aab5ee0f26a9ce5d2e36b55933e97ebad072fe8
abdallah96 <gueye.amadou1996@gmail.com>
2026-01-02 07:52:29 +0100

    fix: configure webpack devtool to resolve Storybook source map errors

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -12,6 +12,10 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.devtool = "eval-cheap-module-source-map";
+    return config;
+  },
 };
 
 export default config;
